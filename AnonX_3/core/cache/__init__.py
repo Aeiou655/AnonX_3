4aab621caca89c791c1fc183b86964a8b316af58
Aeiou655 <rainmoonking@gmail.com>
2026-08-09 16:30:31 +0630

    Restore canonical cache package exports

--- AnonX_3/core/cache/__init__.py
+++ AnonX_3/core/cache/__init__.py
@@ -1,1 +1,23 @@
-"""Direct resolver cache package."""
+"""Canonical playback cache primitives."""
+
+from .hub import CacheEntry, cache_hub
+from .keys import (
+    detect_source,
+    legacy_asset_key,
+    make_cache_key,
+    normalize_lookup_text,
+    parse_cache_key,
+)
+from .states import CacheState, can_transition
+
+__all__ = [
+    "CacheEntry",
+    "cache_hub",
+    "CacheState",
+    "can_transition",
+    "detect_source",
+    "legacy_asset_key",
+    "make_cache_key",
+    "normalize_lookup_text",
+    "parse_cache_key",
+]
